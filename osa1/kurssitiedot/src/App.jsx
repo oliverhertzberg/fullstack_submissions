9f3091b9ed3e9a4f5fa2ebb18ba9eff0de49c071
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2025-07-31 20:12:57 +0300

    step 4

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -23,25 +23,27 @@ const Total = ({e1, e2, e3}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  console.log(part1.exercises)
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  console.log(parts[0].exercises)
 
   return (
     <div>
       <Header course={course}/>
-      <Content  p1={part1.name} ex1={part1.exercises} p2={part2.name} ex2={part2.exercises} p3={part3.name} ex3={part3.exercises}/>
-      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises}/>
+      <Content  p1={parts[0].name} ex1={parts[0].exercises} p2={parts[1].name} ex2={parts[1].exercises} p3={parts[2].name} ex3={parts[2].exercises}/>
+      <Total e1={parts[0].exercises} e2={parts[1].exercises} e3={parts[2].exercises}/>
     </div>
   )
 
