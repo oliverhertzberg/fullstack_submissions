e48b2e42a40e1c2d5c981a7718f6a049c7dc1e43
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2025-08-03 21:54:40 +0300

    2.1 kurssitiedot step 6

---
+++ osa2/kursstiedot/src/Course.jsx
@@ -0,0 +1,36 @@
+const Header = (props) => {
+    return <h1>{props.course}</h1>
+  }
+  
+  const Part = ({part, exercise}) => {
+    return <p>{part} {exercise}</p>
+  }
+  
+  const Content = ({parts}) => {
+    if (!parts) return
+    let result = parts.map((item) =>  {
+    return <Part part={item.name} exercise={item.exercises} key={item.id}/>
+    })
+    return result
+  }
+  
+  const Total = ({parts}) => {
+    if (!parts) return
+    let exercise_count = 0 
+    parts.map((item) => exercise_count+= item.exercises)
+    return <p>Number of exercises {exercise_count}</p>
+  }
+
+const Course = ({course}) => {
+  console.log('course: ',course)
+    if (!course || !course.name) return
+    return (
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
+}
+
+export default Course
