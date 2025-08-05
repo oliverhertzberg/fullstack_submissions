eb831c7bf72fa34a327cdc3113c3fd0f1e2eaaf5
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2025-08-05 23:03:08 +0300

    2.16 puhelinluettelo step11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 import personService from './services/persons'
 
 
@@ -11,13 +12,24 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+  const [notifMessage, setNotifMessage] = useState(null)
+
+  const makeNotification = (message) => {
+    setNotifMessage(message)
+    setTimeout(() => {
+      setNotifMessage(null)
+    }, 5000)
+  }
 
   useEffect(() => {
     personService.getAll()
       .then(res => {
         setPersons(res.data)
       })
-      .catch(error => console.log(error))
+      .catch(error => {
+        console.log(error)
+        makeNotification(`Error: failed to retrieve phonebook contacts!`)
+      })
   },[])
 
   const handleSubmit = (e) => {
@@ -29,14 +41,23 @@ const App = () => {
             personExists.id, 
             {...personExists, number: `${newNumber}`})
           .then(setPersons(persons.map((person) => {
+            makeNotification(`Successfully updated number for ${newName}`)
             return person.id !== personExists.id ? person : {...person, number: `${newNumber}`}
           })))
-          .catch(error => console.log(error))
+          .catch(error => { 
+            console.log(error)
+            makeNotification(`Error: failed to retrieve phonebook contacts!`)
+        })
     } else {
       personService.create({ name: `${newName}`, number: `${newNumber}` }) 
-        .then((res) => setPersons(persons.concat(res.data)))
-        .catch((error) => console.log(error))
-      alert(`contact: ${newName} was submitted successfully!`)
+        .then((res) => {
+          setPersons(persons.concat(res.data))
+          makeNotification(`Contact created!`)
+        })
+        .catch((error) => {
+          console.log(error)
+          makeNotification(`Error: failed to create phonebook contact ${newName}!`)
+        })
     }
     setNewName('')
     setNewNumber('')
@@ -47,8 +68,14 @@ const App = () => {
     console.log(contact)
     if(window.confirm(`Are you sure you want to delete user: ${contact.name}`)) {
       personService.remove(contact.id)
-      .then(setPersons(persons.filter((person) => person.id !== contact.id)))
-      .catch((error) => console.log(error))
+      .then(() => {
+        setPersons(persons.filter((person) => person.id !== contact.id))
+        makeNotification(`Successfully deleted contact!`)
+      })
+      .catch((error) => {
+        console.log(error)
+        makeNotification(`Error: failed to delete contact`)
+      })
     }
   }
 
@@ -64,6 +91,7 @@ const App = () => {
 
   return (
     <div>
+      <Notification message={notifMessage}/>
       <h2>Phonebook</h2>
       <Filter value={nameFilter} onChange={handleFilterChange}/>
       <h3>Add a new contact:</h3>
