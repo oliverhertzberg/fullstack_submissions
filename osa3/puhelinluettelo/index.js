bd87b220ff50e738d93298c272f4721819093c27
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2025-08-12 22:29:17 +0300

    3.1 backend step1

---
+++ osa3/puhelinluettelo/index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = 
+[
+    {
+        "id": "1",
+        "name": "RAT",
+        "number": "444444444"
+    },
+    {
+        "id": "2",
+        "name": "DOG",
+        "number": "777777777"
+    }
+]
+
+
+app.get('/', (requests, response) => {
+    response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/persons', (requests, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    console.log('typeof id = ', typeof(id))
+    const person = persons.find((x) => x.id === id)
+    console.log('person: ', person)
+    
+    if (person)
+        response.json(person)
+    else
+        response.status(404).end()
+})
+
+const generateID = (() => persons.length > 0 ? String(Math.max(...persons.map(c => Number(c.id))) + 1) : 0
+)
+app.post('/api/persons', (request, response) => {
+
+    console.log('request body in post = ', request.body)
+    if (!request.body) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    console.log('headers: ',request.headers)
+    const person = request.body
+    console.log("person in post = ", person)
+    person.id = generateID()
+    persons = persons.concat(person)
+    console.log('persons: ', persons)
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(x => x.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
